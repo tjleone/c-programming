1fec8da44cd89eaa4ba7ad66d2b229650625dcc5
Coursera Learner <nobody@nowhere.nul>
2018-08-25 20:41:16 +0000

    finished 02_code1

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -16,7 +16,7 @@ int main(void) {
   printf("max(33, 0) is %d\n", max(33, 0));
   printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
   //compute the max of 0x451215AF and 0x913591AF and print it out as a decimal number
-  printf("max(0x45121AF, 0x913591AF is %d\n", max(0x451215AF, 0x913591AF));
+  printf("max(0x45121AF, 0x913591AF) is %d\n", max(0x451215AF, 0x913591AF));
   return 0;
 }
 
