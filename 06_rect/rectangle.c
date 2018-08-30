c9a37bb116de59a313d826a62e206b442ff287c0
tjleone <tj@tjleone.com>
2018-08-30 13:09:42 -0500

    finished rectangle.c

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 //I've provided "min" and "max" functions in
 //case they are useful to you
 int min (int a, int b) {
@@ -16,15 +17,39 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rectangle_t {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rectangle_t rectangle;
 
 rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x = r.x + r.width;
+    r.width = abs(r.width);
+  }
+  if (r.height < 0) {
+    r.y = r.y + r.height;
+    r.height = abs(r.height);
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+  rectangle r;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  
+  r.x = max(r1.x, r2.x);
+  r.y = max(r1.y, r2.y);
+  r.width = min(r1.x+r1.width, r2.x+r2.width) - r.x;
+  r.height = min(r1.y+r1.height, r2.y+r2.height) - r.y;
+  if (r.width < 0 || r.height < 0) {
+    r.width = 0;
+    r.height = 0;
+  }
+  return r;
 }
 
 //You should not need to modify any code below this line
