864f5baf392761aa220e2793bdbfc7c18d5d5765
tjleone <tj@tjleone.com>
2018-09-04 13:27:56 -0500

    09_testing

--- 09_testing2/tests.py
+++ 09_testing2/tests.py
@@ -1,4 +1,15 @@
+import random
+
 file = open('tests.txt', 'w')
 for i in range(32):
     for j in range(32):
         file.write(f'{i:05b} {j:05b}\n')
+
+for i in range(64):
+    hand1, hand2 = '', ''
+    for j in range(5):
+        hand1 += chr(random.randrange(256))
+        hand2 += chr(random.randrange(256))
+    file.write(hand1 + ' ' + hand2 + '\n')
+
+file.close()
