37f9cc41ce92202abadaa5365d3614b24e1965a3
tjleone <tj@tjleone.com>
2018-09-04 14:58:24 -0500

    09_testing

--- 09_testing2/tests.py
+++ 09_testing2/tests.py
@@ -1,15 +1,12 @@
 import random
 
 file = open('tests.txt', 'w')
+file.write('12345 123456\n')
+file.write('123456 12345\n')
+file.write('12345 1234\n')
+file.write('1234 12345\n')
 for i in range(32):
     for j in range(32):
         file.write(f'{i:05b} {j:05b}\n')
-
-for i in range(64):
-    hand1, hand2 = '', ''
-    for j in range(5):
-        hand1 += chr(random.randrange(256))
-        hand2 += chr(random.randrange(256))
-    file.write(hand1 + ' ' + hand2 + '\n')
 
 file.close()
