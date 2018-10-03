e01c07e2ac3b748cffafc6633cd90e20f49fd59e
Coursera Learner <nobody@nowhere.nul>
2018-10-03 14:24:05 +0000

    22_tests_power

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -10,20 +10,20 @@ int main(void) {
     unsigned expected;
   } param_t;
 
-  size_t s_params = 15;
+  size_t s_params = 7;
 
   param_t params[] = { {.x = 0, .y = 0, .expected = 1},
 		       {.x = 1, .y = -1, .expected = 1},
 		       {.x = -2, .y = 0, .expected = 1},
 		       {.x = -2, .y = 2, .expected = 4},
 		       {.x = -2, .y = 3, .expected = -8},
-		       {.x = 0, .y = -1, .expected = 1}, // don't know what to expect
-		       {.x = -1, .y = -1} // don't know what to expect
+		       {.x = 0, .y = -1, .expected = 0}, // answer from "correct" implementation
+		       {.x = -1, .y = -1, .expected = -1} // answer from "correct" implementation
   };
 
   for (int i=0; i<s_params; i++) {
     printf("For x=%d, y=%d, expected %d, got %d\n",
-	   i, (int)params[i].expected,
+	   params[i].x, params[i].y, (int)params[i].expected,
 	   (int)power(params[i].x, params[i].y));
     if(power(params[i].x, params[i].y) != params[i].expected) {
       return EXIT_FAILURE;
