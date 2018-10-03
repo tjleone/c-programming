7efced044f515c9ca5dfe6d5785a2951aabfa524
tjleone <tj@tjleone.com>
2018-10-03 13:50:09 -0500

    23_power_rec

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -1,7 +1,7 @@
 
 unsigned power_helper(unsigned x, unsigned y, unsigned ans) {
-  if ((int)y <= 0) {
-    return 1;
+  if (y == 0) {
+    return ans;
   }
   
   return power_helper(x, y-1, ans*x);
@@ -9,7 +9,15 @@ unsigned power_helper(unsigned x, unsigned y, unsigned ans) {
 
 unsigned power(unsigned x, unsigned y) {
   if (x == 1) {
-    return 1;
+    return x;
+  } else if (x == 0) {
+    if (y == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else if ((int)y < 0) {
+    return 0;
   }
   return power_helper(x, y, 1);
 }
