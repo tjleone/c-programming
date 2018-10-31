53544d993a6a668af2fd0559bdb35c5650743e10
tjleone <tj@tjleone.com>
2018-10-31 16:38:46 -0500

    29_outname

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,9 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  const char suffix[] = ".counts";
+  char * outputName = malloc((strlen(inputName) + strlen(suffix) + 1) * sizeof(*outputName));
+  strcpy(outputName, inputName);
+  strcat(outputName, suffix);
+  return outputName;
 }
